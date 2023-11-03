0914b2a43ebec018611cb1b6bc24708369c57e55
Armin Talaie <talaiearmin78@gmail.com>
2023-11-03 00:44:56 -0700

    added missing test endpoints

--- src/routes/(app)/api/test/users/me/+server.ts
+++ src/routes/(app)/api/test/users/me/+server.ts
@@ -2,34 +2,7 @@ import type { IUser } from '$lib/types/User';
 import { json } from '@sveltejs/kit';
 
 export const GET = async () => {
-    const obj: { users: IUser[] } = {
-        users: [
-            {
-                id: 881709496070406145,
-                first_name: "Mike",
-                last_name: "Roberts",
-                pref_name: "Mikert",
-                email: "dev@ubclaunchpad.com",
-                created_at: "2023-07-12T07:30:05.398Z",
-                updated_at: "2023-09-29T21:03:16.349Z",
-                resume_link: "www.google.com",
-                faculty: {
-                    id: 881038255263285249,
-                    label: "Science"
-                },
-                standing: {
-                    id: 881038255380070401,
-                    label: "Other"
-                },
-                specialization: {
-                    id: 881038255685632001,
-                    label: "Other"
-                },
-                roles: [],
-                username: "olti",
-                member_since: "Sept 20, 2023"
-            },
-            {
+    const obj:  IUser  = {
                 id: 881709496070406145,
                 first_name: "Lebron",
                 last_name: "James",
@@ -54,8 +27,8 @@ export const GET = async () => {
                 username: "lbj",
                 member_since: "Sept 1, 2003"
             }
-        ]
-    }
+        
+
 
     return json(obj);
 };
