b3e74ac87ce17a010d059f72575f90cbf8979a12
Sam Zhao <82355500+samzhao273@users.noreply.github.com>
2024-03-10 20:18:29 -0700

    LIve Editor Component - Dynamic Routing File Hiearchy (#56)
    * added live editor component with file hierarchy
    
    * added TODO for marked extension + changed example markdown text
    
    * edited browser issues, componentized the editor
    
    * changed extension lien
    
    * removed file/folder and added style.css for editor component

---
+++ src/lib/components/wiki/DocumentEditor.svelte
@@ -0,0 +1,46 @@
+<script>
+	import { Editor, EditorContent } from 'svelte-tiptap';
+	import StarterKit from '@tiptap/starter-kit';
+	import { onMount } from 'svelte';
+	import { Markdown } from 'tiptap-markdown';
+	import Document from '@tiptap/extension-document';
+	import Heading from '@tiptap/extension-heading';
+	import Paragraph from '@tiptap/extension-paragraph';
+	import Text from '@tiptap/extension-text';
+    import './styles.css';
+	export let area;
+    export let id;
+
+	let initialMarkdownContent =
+		`**Example** Markdown Text This is a paragraph in Markdown. Subheading. 
+		## Heading 1`;
+	/**
+	 * @type {Editor}
+	 */
+	let editor;
+
+	onMount(() => {
+		//   const response = await fetch("./README.md");
+		//   if (response.ok) {
+		// If the response is fine, then populate the markdown and html content
+		// initialMarkdownContent = await response.text();
+		// initialHTMLContent = marked(initialMarkdownContent);
+		//   } else {
+		//     console.error("Failed to fetch README.md");
+		//   }
+		// Properties for new editor
+		editor = new Editor({
+			extensions: [StarterKit, Markdown, Document, Heading, Paragraph, Text],
+			content: initialMarkdownContent,
+			editorProps: {
+				attributes: {
+					class: 'prose prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto focus:outline-none'
+				}
+			}
+		});
+	});
+</script>
+
+<div class="flex flex-col gap-6 flex-1 w-full h-full min-h-[90svh]">
+	<EditorContent {editor} />
+</div>
---
+++ src/routes/(home)/doc/[area]/[...id]/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import DocumentEditor from "$lib/components/wiki/DocumentEditor.svelte";
+
+	export let data;
+</script>
+
+<main>
+	<DocumentEditor area={data.area} id={data.id}/>
+</main>
+
+<style>
+	main {
+		max-width: 800px;
+		margin: 40px auto;
+		padding: 20px;
+		box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+	}
+</style>
